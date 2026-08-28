5aa69f1f57a01899198394c8c88c72d030d7a748
kbladewht <w00110601111@163.com>
2026-08-28 20:33:41 +0800

    add

--- c.sh
+++ c.sh
@@ -2,3 +2,4 @@ export PATH="/c/Users/dellht/AppData/Local/Programs/Python/Python314/Scripts:$PA
 export PATH="/c/Users/dellht/AppData/Local/Programs/Python/Python314:$PATH"
 
 npx tauri build --bundles msi
+npx tauri build --no-bundle
