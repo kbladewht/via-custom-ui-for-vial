db7aeca3eff1597ce77116fa656d5a1ae5b1e6ca
kbladewht <w00110601111@163.com>
2026-09-10 07:18:23 +0800

    add

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,12 @@ export default defineConfig({
   plugins: [react()],
   base: process.env.TAURI_ENV_PLATFORM === undefined ? "/via-custom-ui-for-vial/" : "./",
   define: {
-    "os.EOL": JSON.stringify("\n"),
+    "process.env": {},
   },
   resolve: {
     alias: {
-      os: path.resolve(__dirname, "node_modules/hjson/lib/hjson-common.js"),
+      os: path.resolve(__dirname, "src/shims/os.js"),
+      hjson: path.resolve(__dirname, "node_modules/hjson/bundle/hjson.js"),
       webRawHID:
         process.env.TAURI_ENV_PLATFORM === undefined
           ? path.resolve(__dirname, "src/services/platform/web/webRawHID.ts")
