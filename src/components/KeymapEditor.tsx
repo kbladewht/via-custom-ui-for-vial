ea3e51006ea207b675793a85c33bd96b43dc0138
kbladewht <w00110601111@163.com>
2026-09-10 21:00:08 +0800

    Update KeymapEditor.tsx

--- src/components/KeymapEditor.tsx
+++ src/components/KeymapEditor.tsx
@@ -116,7 +116,7 @@ export function KeymapEditor(props: {
         sx={{
           width: "100%",
           overflowX: "auto",
-          pl: 1,
+          pl: 1.5,
           pr: 1,
         }}
       >
@@ -142,6 +142,7 @@ export function KeymapEditor(props: {
           maxWidth: "100%",
           overflowX: "auto",
           overflowY: focusedKey ? "auto" : "hidden",
+          pl: 1,
           pb: focusedKey ? 3 : 0,
           pt: 0,
           transition: "max-height 220ms ease, margin-top 220ms ease, opacity 180ms ease, transform 220ms ease, visibility 220ms ease, padding-bottom 220ms ease",
